65bd4377f4f8b1fd346f29e0386266d9d34176d6
Andreatorromeo <andreatorromeo@icloud.com>
2020-09-23 15:30:08 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-const hamburger = document.querySelector('.hamburger');
-const navbar = document.querySelector('.navbar');
-const menulist = document.querySelectorAll('.menu>li');
-const home = document.querySelectorAll('.home _header');
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+const menulist = document.querySelectorAll(".menu>li");
+const home = document.querySelectorAll(".home _header");
+const html = document.querySelector("html");
 
-hamburger.addEventListener('click',()=>{
-    navbar.classList.toggle('open');
-})
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("open");
+  disableScroll();
+});
 
-if(home){
-    menulist.forEach(listItem => {
-        listItem.addEventListener('click', ()=>{
-            navbar.classList.toggle('open');
-        })
+if (home) {
+  menulist.forEach((listItem) => {
+    listItem.addEventListener("click", () => {
+      navbar.classList.toggle("open");
+      disableScroll();
     });
+  });
+}
+
+function disableScroll() {
+  html.style.overflow = html.style.overflow === "hidden" ? "visible" : "hidden";
 }
